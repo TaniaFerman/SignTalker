216c06fe33b65ff984f4f467ce95ebbc07f6d05b
norahuang <vac48@hotmail.com>
2016-10-31 22:00:55 -0700

    add cascade classifier

---
+++ cascade_classifiers/haar_training/createnegativelist.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+#  createnegativelist.sh
+#  
+#
+#  Created by Nora Huang on 2016-10-31.
+#
+
+sed '/AL/d' ./negatives.txt > negatives_AL.txt
+sed '/AR/d' ./negatives.txt > negatives_AR.txt
+sed '/BL/d' ./negatives.txt > negatives_BL.txt
+sed '/BR/d' ./negatives.txt > negatives_BR.txt
+sed '/CL/d' ./negatives.txt > negatives_CL.txt
+sed '/CR/d' ./negatives.txt > negatives_CR.txt
+sed '/DL/d' ./negatives.txt > negatives_DL.txt
+sed '/DR/d' ./negatives.txt > negatives_DR.txt
+sed '/EL/d' ./negatives.txt > negatives_EL.txt
+sed '/ER/d' ./negatives.txt > negatives_ER.txt
+sed '/FL/d' ./negatives.txt > negatives_FL.txt
+sed '/FR/d' ./negatives.txt > negatives_FR.txt
+sed '/GL/d' ./negatives.txt > negatives_GL.txt
+sed '/GR/d' ./negatives.txt > negatives_GR.txt
+sed '/HL/d' ./negatives.txt > negatives_HL.txt
+sed '/HR/d' ./negatives.txt > negatives_HR.txt
+sed '/IL/d' ./negatives.txt > negatives_IL.txt
+sed '/IR/d' ./negatives.txt > negatives_IR.txt
+sed '/KL/d' ./negatives.txt > negatives_KL.txt
+sed '/KR/d' ./negatives.txt > negatives_KR.txt
+sed '/LL/d' ./negatives.txt > negatives_LL.txt
+sed '/LR/d' ./negatives.txt > negatives_LR.txt
+sed '/ML/d' ./negatives.txt > negatives_ML.txt
+sed '/MR/d' ./negatives.txt > negatives_MR.txt
+sed '/NL/d' ./negatives.txt > negatives_NL.txt
+sed '/NR/d' ./negatives.txt > negatives_NR.txt
+sed '/OL/d' ./negatives.txt > negatives_OL.txt
+sed '/OR/d' ./negatives.txt > negatives_OR.txt
+sed '/PL/d' ./negatives.txt > negatives_PL.txt
+sed '/PR/d' ./negatives.txt > negatives_PR.txt
+sed '/QL/d' ./negatives.txt > negatives_QL.txt
+sed '/QR/d' ./negatives.txt > negatives_QR.txt
+sed '/RL/d' ./negatives.txt > negatives_RL.txt
+sed '/RR/d' ./negatives.txt > negatives_RR.txt
+sed '/SL/d' ./negatives.txt > negatives_SL.txt
+sed '/SR/d' ./negatives.txt > negatives_SR.txt
+sed '/TL/d' ./negatives.txt > negatives_TL.txt
+sed '/TR/d' ./negatives.txt > negatives_TR.txt
+sed '/UL/d' ./negatives.txt > negatives_UL.txt
+sed '/UR/d' ./negatives.txt > negatives_UR.txt
+sed '/VL/d' ./negatives.txt > negatives_VL.txt
+sed '/VR/d' ./negatives.txt > negatives_VR.txt
+sed '/WL/d' ./negatives.txt > negatives_WL.txt
+sed '/WR/d' ./negatives.txt > negatives_WR.txt
+sed '/XL/d' ./negatives.txt > negatives_XL.txt
+sed '/XR/d' ./negatives.txt > negatives_XR.txt
+sed '/YL/d' ./negatives.txt > negatives_YL.txt
+sed '/YR/d' ./negatives.txt > negatives_YR.txt
