653dd858702e1e5be58830df4f4b4346127e80c2
tristanp2 <tristanp2@hotmail.com>
2016-11-08 14:43:15 -0800

    added app project files

---
+++ SignCoachApp/AppAttempt/app/src/main/java/com/example/tristan/appattempt/FileOps.java
@@ -0,0 +1,57 @@
+package com.example.tristan.appattempt;
+
+import android.util.Log;
+
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Locale;
+
+public class FileOps {
+
+	// Returns a hashset of strings with all the mounted storage paths
+	public static HashSet<String> getExternalMounts() {
+		final HashSet<String> out = new HashSet<String>();
+		String reg = "(?i).*vold.*(vfat|ntfs|exfat|fat32|ext3|ext4).*rw.*";
+		String s = "";
+		try {
+			final Process process = new ProcessBuilder().command("mount")
+					.redirectErrorStream(true).start();
+			process.waitFor();
+			final InputStream is = process.getInputStream();
+			final byte[] buffer = new byte[1024];
+			while (is.read(buffer) != -1) {
+				s = s + new String(buffer);
+			}
+			is.close();
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
+
+		// parse output
+		final String[] lines = s.split("\n");
+		for (String line : lines) {
+			if (!line.toLowerCase(Locale.US).contains("asec")) {
+				if (line.matches(reg)) {
+					String[] parts = line.split(" ");
+					for (String part : parts) {
+						if (part.startsWith("/"))
+							if (!part.toLowerCase(Locale.US).contains("vold"))
+								out.add(part);
+					}
+				}
+			}
+		}
+		return out;
+	}
+
+	// Returns the external storage path or an empty string if none found
+	public static String getStoragePath() {
+		String result = "";
+		HashSet<String> mounts = getExternalMounts();
+		for (String s : mounts) {
+			result = s;
+		}
+
+		return result;
+	}
+}
---
+++ SignCoachApp/AppAttempt/app/src/main/java/com/example/tristan/appattempt/LearnMessage.java
@@ -0,0 +1,23 @@
+package com.example.tristan.appattempt;
+
+/**
+ * Created by Tristan on 2016-11-07.
+ */
+
+public class LearnMessage {
+    private boolean message_type = false;    //false for question, true for lesson
+    private char character;
+    LearnMessage(boolean type, char character){
+        this.character = character;
+        this.message_type = type;
+    }
+    public boolean isLesson(){
+        return message_type;
+    }
+    public String getString(){
+        return Character.toString(character);
+    }
+    public char getChar(){
+        return character;
+    }
+}
